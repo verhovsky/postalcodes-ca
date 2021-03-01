0a02a16f36ec846a30a732729340e2021ce33979
boris <boris@verhovs.ky>
2021-03-01 14:59:38 -0500

    implement actual postal codes
    changed license to MIT after discussion with the
    author of pyzipcode https://github.com/vangheem/pyzipcode/issues/5
    
    added CI
    
    numerous readme changes

--- postalcodes_ca/__init__.py
+++ postalcodes_ca/__init__.py
@@ -4,16 +4,115 @@ import string
 import re
 import time
 from collections import namedtuple
+from collections.abc import Mapping
 from math import degrees, sin, asin, cos, radians
 
 from .settings import db_location
 
 
-class ConnectionManager:
-    """
-    Assumes a database that will work with cursor objects
-    """
+def parse_fsa(fsa, strict=False):
+    if not strict:
+        fsa = fsa.upper()
 
+    if len(fsa) < 3:
+        raise ValueError(f"invalid FSA, must be 3 characters: {str(fsa)!r}")
+    if strict and len(fsa) > 3:
+        raise ValueError(f"invalid FSA, must be 3 characters: {str(fsa)!r}")
+
+    if fsa[0] not in POSTAL_CODE_FIRST_LETTER_ALPHABET:
+        raise ValueError(f"invalid FSA, must start with one of {POSTAL_CODE_FIRST_LETTER_ALPHABET}: {str(fsa)!r}")
+    if fsa[1] not in string.digits:
+        raise ValueError(f"invalid FSA, second character must be a digit: {str(fsa)!r}")
+    if fsa[2] not in POSTAL_CODE_ALPHABET:
+        raise ValueError(f"invalid FSA, third character must be one of {POSTAL_CODE_ALPHABET}: {str(fsa)!r}")
+
+    return fsa[:3]
+
+def parse_postal_code(pc, strict=False):
+    if not strict:
+        pc = pc.upper()
+
+    if strict:
+        if len(pc) != 7:
+            raise ValueError(f"invalid postal code, must be 7 characters: {str(pc)!r}")
+    else:
+        if len(pc) < 6:
+            raise ValueError(f"invalid postal code, too short: {str(pc)!r}")
+        if ' ' in pc[:7] and len(pc) < 7:
+            raise ValueError(f"invalid postal code, too short: {str(pc)!r}")
+
+    try:
+        fsa = parse_fsa(pc[:3], strict=strict)
+    except ValueError as fsa_error:
+        raise ValueError(f"invalid postal code, " + str(fsa_error))
+
+    if strict and pc[3] != ' ':
+        raise ValueError(f"invalid postal code, must include a space: {str(pc)!r}")
+
+    ldu = pc[4:7] if pc[3] == ' ' else pc[3:6]
+
+    if ldu[0] not in string.digits:
+        raise ValueError(f"invalid postal code, fourth character must be a digit: {str(pc)!r}")
+    if ldu[1] not in POSTAL_CODE_ALPHABET:
+        raise ValueError(f"invalid postal code, fifth character must be one of {POSTAL_CODE_ALPHABET}: {str(pc)!r}")
+    if ldu[2] not in string.digits:
+        raise ValueError(f"invalid postal code, sixth character must be a digit: {str(pc)!r}")
+
+    return fsa + ' ' + ldu
+
+
+@dataclass
+class Code:
+    """A base class used for postal codes and FSA codes"""
+    code: str
+    name: str
+    province: str
+    latitude: float
+    longitude: float
+
+    def _parse(self, fsa):
+        raise NotImplementedError
+
+    @property
+    def is_valid(self):
+        try:
+            self._parse(self.fsa)
+            return True
+        except ValueError:
+            return False
+
+    @property
+    def postal_district(self):
+        return self.code[0]
+
+    @property
+    def fsa(self):
+        return self.code[:3]
+
+    @property
+    def is_rural(self):
+        # TODO: are there any exceptions?
+        return self.code[1] == "0"
+
+@dataclass
+class FSA(Code):
+    """The first 3 characters of a Canadian postal code"""
+    accuracy: int
+
+    _parse = parse_fsa
+
+@dataclass
+class PostalCode(Code):
+    """A 6 character Canadian postal code"""
+    _parse = parse_postal_code
+
+    @property
+    def accuracy(self):
+        return 6
+
+
+
+class ConnectionManager:
     def __init__(self):
         # test out the connection...
         conn = sqlite3.connect(db_location)
@@ -37,73 +136,48 @@ class ConnectionManager:
         conn.close()
         return res
 
-PC_QUERY = "SELECT * FROM FSACodes WHERE fsa=?"
-PC_RANGE_QUERY = "SELECT * FROM FSACodes WHERE longitude >= ? and longitude <= ? AND latitude >= ? and latitude <= ?"
-PC_FIND_QUERY = "SELECT * FROM FSACodes WHERE fsa LIKE ? AND name LIKE ? AND province LIKE ?"
+QUERY = "SELECT * FROM {table_name} WHERE code=?"
+RANGE_QUERY = "SELECT * FROM {table_name} WHERE longitude >= ? and longitude <= ? AND latitude >= ? and latitude <= ?"
+FIND_QUERY = "SELECT * FROM {table_name} WHERE code LIKE ? AND name LIKE ? AND province LIKE ?"
+ALL_QUERY = "SELECT * FROM {table_name}"
+LEN_QUERY = "SELECT COUNT(*) FROM {table_name}"
 
 POSTAL_CODE_ALPHABET = 'ABCEGHJKLMNPRSTVWXYZ'  # postal codes don't use D, F, I, O, Q or U
 POSTAL_CODE_FIRST_LETTER_ALPHABET = 'ABCEGHJKLMNPRSTVXY'  # additionally, the first letter doesn't use W or Z
 
+# TODO: check if using sets for the above improves performance
 
-@dataclass
-class FSACode:
-    """The first 3 characters of a postal code"""
-    fsa: str
-    name: str
-    province: str
-    latitude: float
-    longitude: float
-    accuracy: int
-
-    @property
-    def is_valid(self):
-        try:
-            validate_fsa(self.fsa)
-            return True
-        except ValueError:
-            return False
-
-def format_result(codes):
-    if codes:
-        return [FSACode(*code) for code in codes]
-    return None
-
-def validate_fsa(fsa, strict=False):
-    if not strict:
-        fsa = fsa.upper()
-
-    if len(fsa) < 3:
-        raise ValueError(f"invalid FSA, must be 3 characters: {str(fsa)!r}")
-    if strict and len(fsa) > 3:
-        raise ValueError(f"invalid FSA, must be 3 characters: {str(fsa)!r}")
-
-    if fsa[0] not in POSTAL_CODE_FIRST_LETTER_ALPHABET:
-        raise ValueError(f"invalid FSA, must start with one of {POSTAL_CODE_FIRST_LETTER_ALPHABET}: {str(fsa)!r}")
-    if fsa[1] not in string.digits:
-        raise ValueError(f"invalid FSA, second character must be a digit: {str(fsa)!r}")
-    if fsa[2] not in POSTAL_CODE_ALPHABET:
-        raise ValueError(f"invalid FSA, third character must be one of {POSTAL_CODE_ALPHABET}: {str(fsa)!r}")
-
-    return fsa[:3]
 
 class CodeNotFoundException(Exception):
     pass
 
-class FSACodeNotFoundException(CodeNotFoundException):
+class FSANotFoundException(CodeNotFoundException):
     pass
 
-class FSACodeDatabase:
+class PostalCodeNotFoundException(CodeNotFoundException):
+    pass
+
+class CodeDatabase(Mapping):
+    result_type = Code
+    not_found_exception = CodeNotFoundException
+    parse = lambda x: x
+
     def __init__(self, conn_manager=None):
         if conn_manager is None:
             conn_manager = ConnectionManager()
         self.conn_manager = conn_manager
 
-    def get_nearby(self, fc, radius):
+    def _format_result(self, codes):
+        if codes:
+            return [self.result_type(*code) for code in codes]
+        return None
+
+    def get_nearby(self, code, radius):
         # TODO: this is a square, not a radius.
         # use this: https://stackoverflow.com/a/39298241
-        fc = self.get(fc)
-        if fc is None:
-            raise FSACodeNotFoundException("Could not find FSA code " + str(fc))
+        code = self.get(code)
+        if code is None:
+            raise self.not_found_exception("Could not find code " + str(code))
 
         radius = float(radius)
 
@@ -112,28 +186,30 @@ class FSACodeDatabase:
         '''
         earth_radius  = 6371
         dlat = radius / earth_radius
-        dlon = asin(sin(dlat) / cos(radians(fc.latitude)))
+        dlon = asin(sin(dlat) / cos(radians(code.latitude)))
         lat_delta = degrees(dlat)
         lon_delta = degrees(dlon)
 
         if lat_delta < 0:
-            lat_range = (fc.latitude + lat_delta, fc.latitude - lat_delta)
+            lat_range = (code.latitude + lat_delta, code.latitude - lat_delta)
         else:
-            lat_range = (fc.latitude - lat_delta, fc.latitude + lat_delta)
+            lat_range = (code.latitude - lat_delta, code.latitude + lat_delta)
 
-        long_range  = (fc.longitude - lat_delta, fc.longitude + lon_delta)
+        long_range  = (code.longitude - lat_delta, code.longitude + lon_delta)
 
-        return format_result(self.conn_manager.query(PC_RANGE_QUERY , (
+        # TODO: return empty list instead of None?
+        return self._format_result(self.conn_manager.query(self.RANGE_QUERY, (
             long_range[0], long_range[1],
             lat_range[0], lat_range[1]
         )))
 
-    def search(self, fsa=None, name=None, province=None):
-        if fsa is None:
-            fsa = "%"
+    def search(self, code=None, name=None, province=None):
+        # TODO: allow passing an FSA/PostalCode object?
+        if code is None:
+            code = "%"
         else:
             # TODO: validate?
-            fsa = fsa.upper()
+            code = code.upper()
 
         if name is None:
             name = "%"
@@ -145,26 +221,67 @@ class FSACodeDatabase:
         else:
             province = province.upper()
 
-        return format_result(self.conn_manager.query(PC_FIND_QUERY , (fsa, name, province)))
+        # TODO: return empty list instead of None?
+        return self._format_result(self.conn_manager.query(self.FIND_QUERY , (code, name, province)))
 
-    def get(self, fc, default=None, strict=True):
-        if isinstance(fc, FSACode):
-            fc = fc.fsa
-        if not isinstance(fc, str):
-            raise TypeError('expected string, got "{type(fc)}"')
-        fc = validate_fsa(fc, strict)
-        results = format_result(self.conn_manager.query(PC_QUERY , (fc,)))
-
+    def get(self, code, default=None, strict=True):
+        if isinstance(code, self.result_type):
+            code = code.code
+        if not isinstance(code, str):
+            raise TypeError('expected string or {self.result_type}, got "{type(code)}"')
+        code = self.parse(code, strict)
+        results = self._format_result(self.conn_manager.query(self.QUERY, (code,)))
+        if results is None:
+            return default
         if len(results) > 1:
-            raise ValueError(f"looking up {fc!r} returned {len(results)} results")
-        if results:
-            return results[0]
-        return default
+            raise ValueError(f"looking up {code!r} returned {len(results)} results")
+        return results[0]
 
-    def __getitem__(self, fc):
-        res = self.get(fc)
+    def __getitem__(self, code):
+        res = self.get(code)
         if res is None:
-            raise KeyError(fc)
+            raise KeyError(code)
         return res
 
-fsa_codes = FSACodeDatabase()
+    def __iter__(self):
+        results = self.conn_manager.query(self.ALL_QUERY)
+        for res in results:
+            yield self.result_type(*res)
+
+    def __len__(self):
+        return self.conn_manager.query(self.LEN_QUERY)[0][0]
+
+
+class FSADatabase(CodeDatabase):
+    result_type = FSA
+    not_found_exception = FSANotFoundException
+
+    def parse(self, *args, **kwargs):
+        return parse_fsa(*args, **kwargs)
+
+    QUERY = QUERY.format(table_name="FSACodes")
+    RANGE_QUERY = RANGE_QUERY.format(table_name="FSACodes")
+    FIND_QUERY = FIND_QUERY.format(table_name="FSACodes")
+    ALL_QUERY = ALL_QUERY.format(table_name="FSACodes")
+    LEN_QUERY = LEN_QUERY.format(table_name="FSACodes")
+
+
+class PostalCodeDatabase(CodeDatabase):
+    result_type = PostalCode
+    not_found_exception = PostalCodeNotFoundException
+
+    def parse(self, *args, **kwargs):
+        return parse_postal_code(*args, **kwargs)
+
+    QUERY = QUERY.format(table_name="PostalCodes")
+    RANGE_QUERY = RANGE_QUERY.format(table_name="PostalCodes")
+    FIND_QUERY = FIND_QUERY.format(table_name="PostalCodes")
+    ALL_QUERY = ALL_QUERY.format(table_name="PostalCodes")
+    LEN_QUERY = LEN_QUERY.format(table_name="PostalCodes")
+
+
+fsa_codes = FSADatabase()
+postal_codes = PostalCodeDatabase()
+
+# TODO: unified database that can return either?
+# codes = CodesDatabase()
--- postalcodes_ca/import.py
+++ postalcodes_ca/import.py
@@ -8,77 +8,143 @@ try:
 except:
     from postalcodes_ca.settings import db_location
 
+FSA_FILE = "CA.tsv"
+POSTAL_CODES_FILE = "CA_full.txt"
+
+
+def log_error(msg, row, row_idx=None):
+    if row_idx is None:
+        msg = f"{msg}: {row!r}"
+    else:
+        msg = f"{msg}: row {row_idx: <6} {row!r}"
+    print(msg, file=sys.stderr)
+
+def read_codes(filename):
+    codes = {}
+    with open(filename, newline="", encoding="utf-8") as f:
+        reader = csv.reader(f, delimiter="\t")
+        for idx, row in enumerate(reader, start=1):
+            # country code      : iso country code, 2 characters
+            # postal code       : varchar(20)
+            # place name        : varchar(180)
+            # admin name1       : 1. order subdivision (state) varchar(100)
+            # admin code1       : 1. order subdivision (state) varchar(20)
+            # admin name2       : 2. order subdivision (county/province) varchar(100)
+            # admin code2       : 2. order subdivision (county/province) varchar(20)
+            # admin name3       : 3. order subdivision (community) varchar(100)
+            # admin code3       : 3. order subdivision (community) varchar(20)
+            # latitude          : estimated latitude (wgs84)
+            # longitude         : estimated longitude (wgs84)
+            # accuracy          : accuracy of lat/lng from 1=estimated, 4=geonameid, 6=centroid of addresses or shape
+
+            # TODO: use administrative subdivisions in FSA data.
+            # The 3rd level is used in Quebec.
+            # For the other provinces, the second level usually holds just the city/town
+            # name. The "code" for those is some 7 digit number I've never seen before.
+            #
+            # Unlike the FSA data, for postal codes "province_code" is a number "01"
+            # for Albera, etc. You can see all of them with
+            # xsv select 4,5 -d'\t' CA_full.txt  | sort | uniq
+            # The admininstrative divisions (the 4 ignored fields) are always empty,
+            # which you can verify with
+            # xsv select 6,7,8,9 -d'\t' CA_full.txt  | sort | uniq
+            country_code, code, name, province, province_code, _, _, _, _, lat, longt, accuracy, = row
+
+            if country_code != "CA":
+                log_error(f"country code {country_code!r} isn't 'CA'", row, idx)
+                sys.exit(1)
+
+            expected_len = 3 if filename == FSA_FILE else 7
+            if len(code) > expected_len:
+                log_error(f"code is too long  {code!r}", row, idx)
+                continue
+            if len(code) < expected_len:
+                log_error(f"code is too short {code!r}", row, idx)
+                continue
+
+            for var, val in [("code", code), ("name", name), ("province", province), ("province_code", province_code), ("lat", lat), ("longt", longt)]:
+                if val != val.strip():
+                    log_error(f"{var} contains whitespace", row, idx)
+                if not val:
+                    log_error(f"missing {var}", row, idx)
+
+            # TODO: report and fix this upstream
+            if "Notre-Dame-de-GrÔce" in name:
+                new_name = name.replace("Notre-Dame-de-GrÔce", "Notre-Dame-de-Grâce")
+                log_error(f"bad name {name!r}, changing to {new_name!r}", row, idx)
+                name = new_name
+
+            # https://en.wikipedia.org/wiki/List_of_extreme_points_of_Canada
+            # converted to decimal with https://www.fcc.gov/media/radio/dms-decimal
+            # TODO: check that coords are in the FSA using the census shapefile
+            lat, longt = float(lat), float(longt)
+            if not (41.681389 <= lat <= 83.111389):
+                log_error(f"out of bounds latitude {lat} {longt}", row, idx)
+            if not (-141.001944 <= longt <= -52.619444):
+                log_error(f"out of bounds longitude {lat} {longt}", row, idx)
+
+            if not accuracy:
+                log_error("missing accuracy", row, idx)
+                accuracy = None
+            else:
+                accuracy = int(accuracy)
+                if accuracy not in range(1, 7):
+                    log_error(f"invalid accuracy {accuracy}", row, idx)
+                    # accuracy = None
+            if filename == POSTAL_CODES_FILE:
+                if accuracy != 6:
+                    log_error("accuracy for postal codes must be '6'", row, idx)
+
+            if code in codes:
+                diff_vals = sum(orig != dup for dup, orig in zip((code, name, province, lat, longt, accuracy), codes[code]))
+                if diff_vals:
+                    log_error(f'duplicate with {diff_vals} different value(s) {codes[code]!r}', row, idx)
+                    # sys.exit(1)
+            else:
+                codes[code] = (code, name, province, lat, longt, accuracy)
+    return codes.values()
+
+os.remove(db_location)
 conn = sqlite3.connect(db_location)
 c = conn.cursor()
-
 c.execute("DROP TABLE IF EXISTS FSACodes;")
 c.execute("""\
 CREATE TABLE FSACodes(
-    fsa VARCHAR(3) NOT NULL,
+    code VARCHAR(3) NOT NULL,
     name VARCHAR(180) NOT NULL,
     province VARCHAR(100) NOT NULL,
+    -- province_code VARCHAR(2) NOT NULL,
     latitude DOUBLE NOT NULL,
     longitude DOUBLE NOT NULL,
     accuracy INT
 );""")
-c.execute("CREATE INDEX fsa_index ON FSACodes(fsa);")
-c.execute("CREATE INDEX name_index ON FSACodes(name);")
-c.execute("CREATE INDEX province_index ON FSACodes(province);")
+c.execute("CREATE INDEX fsa_code_index ON FSACodes(code);")
+c.execute("CREATE INDEX fsa_name_index ON FSACodes(name);")
+c.execute("CREATE INDEX fsa_province_index ON FSACodes(province);")
 
+for row in read_codes(FSA_FILE):
+    c.execute("INSERT INTO FSACodes values(?,?,?,?,?,?)", row)
 
-with open("CA.tsv", newline="", encoding="utf-8") as f:
-    reader = csv.reader(f, delimiter="\t")
-    for idx, row in enumerate(reader, start=1):
-        # country code      : iso country code, 2 characters
-        # postal code       : varchar(20)
-        # place name        : varchar(180)
-        # admin name1       : 1. order subdivision (state) varchar(100)
-        # admin code1       : 1. order subdivision (state) varchar(20)
-        # admin name2       : 2. order subdivision (county/province) varchar(100)
-        # admin code2       : 2. order subdivision (county/province) varchar(20)
-        # admin name3       : 3. order subdivision (community) varchar(100)
-        # admin code3       : 3. order subdivision (community) varchar(20)
-        # latitude          : estimated latitude (wgs84)
-        # longitude         : estimated longitude (wgs84)
-        # accuracy          : accuracy of lat/lng from 1=estimated, 4=geonameid, 6=centroid of addresses or shape
+c.execute("DROP TABLE IF EXISTS PostalCodes;")
+c.execute("""\
+CREATE TABLE PostalCodes(
+    code VARCHAR(7) NOT NULL,
+    name VARCHAR(180) NOT NULL,
+    province VARCHAR(100) NOT NULL,
+    latitude DOUBLE NOT NULL,
+    longitude DOUBLE NOT NULL
+);""")
+# These indeces double the file size of postalcodes.db
+c.execute("CREATE INDEX postal_code_index ON PostalCodes(code);")
+c.execute("CREATE INDEX postal_name_index ON PostalCodes(name);")
+c.execute("CREATE INDEX postal_province_index ON PostalCodes(province);")
 
-        # TODO: use administrative subdivisions. The 3rd level is used in Quebec
-        # For the other provinces, the second level usually holds the city/town
-        # the "code" for those is some 7 digit number I've never seen before.
-        country_code, fsa, name, province, province_code, _, _, _, _, lat, longt, accuracy, = row
-
-        if country_code != "CA":
-            sys.exit(f"invalid country code {country_code!r} in row {idx}")
-
-        for var, val in [("fsa", fsa), ("name", name), ("province", province), ("province_code", province_code), ("lat", lat), ("longt", longt)]:
-            if val != val.strip():
-                print(var, "contains whitespace:", fsa, province_code, name, file=sys.stderr)
-            if not val:
-                print("missing", var + ":", fsa, province_code, name, file=sys.stderr)
-
-        # https://en.wikipedia.org/wiki/List_of_extreme_points_of_Canada
-        # converted to decimal with https://www.fcc.gov/media/radio/dms-decimal
-        longt, lat = float(longt), float(lat)
-        if not (41.681389 <= lat <= 83.111389):
-            print(f"out of bounds latitude {lat}:", fsa, province_code, name, file=sys.stderr)
-        if not (-141.001944 <= longt <= -52.619444):
-            print(f"out of bounds longitude {longt}:", fsa, province_code, name, file=sys.stderr)
-
-        if not accuracy:
-            print("missing accuracy:", fsa, province_code, name, file=sys.stderr)
-            accuracy = None
-        else:
-            accuracy = int(accuracy)
-            if accuracy not in range(1, 7):
-                print(f"invalid accuracy {accuracy}:", fsa, province_code, name, file=sys.stderr)
-                # accuracy = None
-
-        c.execute(
-            "INSERT INTO FSACodes values(?,?,?,?,?,?)",
-            (fsa, name, province, lat, longt, accuracy),
-        )
+postal_codes = read_codes(POSTAL_CODES_FILE)
+# Santa's postal code is missing from the postal codes but not from the FSA codes
+SANTA = ('H0H 0H0', 'Reserved (Santa Claus)', 'Quebec', 90, 0, 6)
+for postal_code in list(postal_codes) + [SANTA]:
+    # don't include accuracy, it's always 6
+    c.execute("INSERT INTO PostalCodes values(?,?,?,?,?)", postal_code[:-1])
 
 conn.commit()
-
-# We can also close the cursor if we are done with it
 c.close()
